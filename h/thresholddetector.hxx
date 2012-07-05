8621a3d9a33bb777e2ac2430b8a1d85a398d90ad
jacob <jacob@6a9d50ae-9395-4028-9bd6-dec3b7c651a2>
2012-07-05 02:07:13 +0000

    Changed (string) parameter handling. Added threshold detector. Added time delay. Added stub for ML neuron.
    git-svn-id: file:///home/jacob/subversion/libneurolab@22 6a9d50ae-9395-4028-9bd6-dec3b7c651a2

---
+++ h/thresholddetector.hxx
@@ -0,0 +1,60 @@
+/* Copyright Information
+__________________________________________________________________________
+
+Copyright (C) 2005 Jacob Kanev
+
+This file is part of NeuroLab.
+
+NeuroLab is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
+__________________________________________________________________________
+*/
+
+/// Detects whether a value is above a threshold
+/*! Simple threshold detector. Receives a stochastic variable and a 
+threshold value at construction time,
+and has an event whenever the current value of the variable exceeds
+the threshold. */
+
+#include <stochastic.hxx>
+
+class ThresholdDetector : public StochasticEventGenerator
+{
+private:
+	StochasticVariable *detectorSource;
+	double detectorThreshold;
+	bool detectorBelow;
+	
+public:
+	/// Construction
+	
+	ThresholdDetector( StochasticVariable *source, double threshold, class Time *time, bool below=false, const string& name="", const string& type="Event Generator" )
+	: StochasticEventGenerator(time, name, type)
+	{
+		detectorSource = source;
+		detectorThreshold = threshold;
+		detectorBelow = below;
+	}
+	
+	/// Destruction
+	virtual ~ThresholdDetector() {}
+	
+	/// has event?
+	bool hasEvent() {
+		if (detectorBelow)
+			return (detectorSource->getCurrentValue() < detectorThreshold);
+		else
+			return (detectorSource->getCurrentValue() > detectorThreshold);
+	}
+};
